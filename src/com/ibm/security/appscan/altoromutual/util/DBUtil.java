0f861814f111be5b58807f8ed4acbee1bed9621a
jrocia <69405400+jrocia@users.noreply.github.com>
2026-02-02 17:35:14 -0300

    Remove duplicate ResultSet execution in DBUtil
    Removed duplicate query execution for user authentication.

--- src/com/ibm/security/appscan/altoromutual/util/DBUtil.java
+++ src/com/ibm/security/appscan/altoromutual/util/DBUtil.java
@@ -217,7 +217,7 @@ public class DBUtil {
 		Statement statement = connection.createStatement();
 		
 		ResultSet resultSet =statement.executeQuery("SELECT COUNT(*)FROM PEOPLE WHERE USER_ID = '"+ user +"' AND PASSWORD='" + password + "'"); /* BAD - user input should always be sanitized */
-		
+		ResultSet resultSet =statement.executeQuery("SELECT COUNT(*)FROM PEOPLE WHERE USER_ID = '"+ user +"' AND PASSWORD='" + password + "'"); /* BAD - user input should always be sanitized */
 		if (resultSet.next()){
 			
 				if (resultSet.getInt(1) > 0)
